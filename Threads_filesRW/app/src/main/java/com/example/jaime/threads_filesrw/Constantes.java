28a438ad32feaade6b6c94d398004e5b54320aa0
Jaime Cárdenas Mesa <jaime.cardenas176@gmail.com>
2018-01-16 01:01:00 +0100

    metodo de escritura de ficheros

---
+++ Threads_filesRW/app/src/main/java/com/example/jaime/threads_filesrw/Constantes.java
@@ -0,0 +1,10 @@
+package com.example.jaime.threads_filesrw;
+
+/**
+ * Created by jaime on 16/01/2018.
+ */
+
+public class Constantes {
+
+
+}
--- Threads_filesRW/app/src/main/java/com/example/jaime/threads_filesrw/MainActivity.java
+++ Threads_filesRW/app/src/main/java/com/example/jaime/threads_filesrw/MainActivity.java
@@ -2,12 +2,55 @@ package com.example.jaime.threads_filesrw;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button buttonEscribe;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonEscribe = findViewById(R.id.buttonEscribe);
+    }
+
+
+    public void escribeArchivo(View view) {
+
+
+        new Thread(new Runnable() {
+            public void run() {
+                BufferedWriter escritor = null;
+            try{
+                escritor = new BufferedWriter( new OutputStreamWriter(
+                        openFileOutput("archivo_prueba.txt", MODE_PRIVATE)
+                ));
+                String linea = null;
+                for (int i=0; i<100; i++){
+                    escritor.write(i+"\n");
+                }
+            }catch (FileNotFoundException e) {
+                System.err.println("no se encuentra el fichero o está corrupto");
+                e.printStackTrace();
+            } catch (IOException e) {
+                System.err.println("error de entradas/salidas");
+                e.printStackTrace();
+            } finally {
+                if (escritor != null)
+                    try {
+                        escritor.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+            }
+
+            }
+        }).start();
     }
 }
