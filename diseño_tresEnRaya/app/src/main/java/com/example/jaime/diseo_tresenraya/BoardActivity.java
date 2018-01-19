ee3e440e50faba6aac1a1d5c31047e5dec1c9d38
jaime cardenas <jaime.cardenas176@gmail.com>
2018-01-19 11:54:01 +0100

    poniendo al dia el repositorio de clase

---
+++ diseño_tresEnRaya/app/src/main/java/com/example/jaime/diseo_tresenraya/BoardActivity.java
@@ -0,0 +1,18 @@
+package com.example.jaime.diseo_tresenraya;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_board);
+
+        Bundle extras = getIntent().getExtras();
+
+        String p1Name = extras.getString(Constantes.EXTRA_PLAYER_1);
+        String p2Name = extras.getString(Constantes.EXTRA_PLAYER_2);
+    }
+}
---
+++ diseño_tresEnRaya/app/src/main/java/com/example/jaime/diseo_tresenraya/Constantes.java
@@ -0,0 +1,11 @@
+package com.example.jaime.diseo_tresenraya;
+
+/**
+ * Created by jaime on 19/01/18.
+ */
+
+public class Constantes {
+
+    public static final String EXTRA_PLAYER_1 = "player1";
+    public static final String EXTRA_PLAYER_2 = "player2";
+}
